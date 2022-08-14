9bf1b85747cbfdef7f4aa83f15fc4ae62b04d7cc
Altarmiz Tutorials <110947525+Altarmiz@users.noreply.github.com>
2022-08-14 21:18:11 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
+// Lets create some spans inside '.animated-bg' div
+
+// first, select all the div with class '.animated-bg'
 const animatedBg = document.querySelectorAll('.animated-bg')
 
+// Now, run a foreach loop and create + append those spans inside every selected div
 animatedBg.forEach(bg => {
+  // lets parse the value from the 'data-amount' attribute
   amount = parseInt(bg.getAttribute('data-amount'))
 
-  for (let index = 0; index < amount; index++) {
+  // now run a 'for loop', to create a span every time and append it into the current div
+  for (let i = 0; i < amount; i++) {
     const span = document.createElement('span')
-
     bg.appendChild(span)
   }
-})
-
-document.querySelectorAll('.btn').forEach(btn => {
-  btn.addEventListener('click', () => {
-    console.log('Btn Clicked')
-  })
 })
